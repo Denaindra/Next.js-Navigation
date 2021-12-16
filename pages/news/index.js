e90305adaae2dc1c0e5cb94d8c2708d008d68a24
gayan <mgdperera@gmail.com>
2021-12-16 08:59:17 +0530

    add page link between pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,23 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 function Index() {
-    return(<h1>this is news index page</h1>);
+  return (
+    <Fragment>
+      <h1>This is News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/coffe-news">Coffee</Link>
+        </li>
+        <li>
+          <Link href="/news/tea-news">Tea</Link>
+        </li>
+        <li>
+          <Link href="/news/milk-news">Milk</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default Index;
