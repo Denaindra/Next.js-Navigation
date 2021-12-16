df91958cd6ccc81f9aed524c7892d7ae96217745
gayan <mgdperera@gmail.com>
2021-12-16 07:42:15 +0530

    add nested page navigation

--- pages/news.js
+++
@@ -1,5 +0,0 @@
-function News() {
-    return(<h1>This is new page</h1>);
-}
-
-export default News;
---
+++ pages/news/index.js
@@ -0,0 +1,5 @@
+function Index() {
+    return(<h1>this is news index page</h1>);
+}
+
+export default Index;
